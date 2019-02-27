5ba640bbf6dc4971e7e20704aa9c12bfc7d41249
Dev Paudel <dev.paudel@ufl.edu>
2019-02-27 12:18:37 -0500

    Create copy2clipboard.R

---
+++ R/copy2clipboard.R
@@ -0,0 +1,6 @@
+#### Copy to excel ####
+copy2clipboard <- function(df, sep="\t", dec=".", max.size=(200*1000)){
+  # Copy a data.frame to clipboard
+  write.table(df, paste0("clipboard-", formatC(max.size, format="f", digits=0)), sep=sep, row.names=TRUE, col.names=NA, dec=dec)
+}
+#usage: copy2clipboard(df) and then Ctrl+V to paste
