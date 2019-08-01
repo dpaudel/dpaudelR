0ff03e31dfc3ec135116538caa45e2389bfdf9a7
Dev Paudel <dev.paudel@ufl.edu>
2019-08-01 11:31:03 -0400

    Create anova_dev.R

---
+++ R/anova_dev.R
@@ -0,0 +1,47 @@
+# anova_dev("Petal.Length~Species", iris, "Petal.Length", "Species")
+anova_dev <- function (user_model, user_data, user_treatment, user_group){
+  model1a <- as.formula(user_model)
+  aov1 <- aov(model1a, data=user_data)
+  print("ANOVA table")
+  print(summary(aov1))
+  
+  # Tukey HSD
+  hsd_aov1 <- HSD.test(aov1, user_group, group=TRUE)
+  
+  # Sort
+  order_aov1 <- hsd_aov1$groups[order(rownames(hsd_aov1$groups)),]
+  
+  # Add species column
+  order_aov1 <- order_aov1 %>% mutate(Name=rownames(order_aov1))
+  
+  # Get summary statistics from Rmisc package
+  data1a <- user_data
+  data_stats <- summarySE(data1a, measurevar= user_treatment , groupvars= user_group, na.rm=TRUE)
+  
+  # Merge HSD grouping and summary statistics
+  
+  data_merged <- left_join(data_stats, order_aov1)
+  print("HSD ranking")
+  print(data_merged)
+  # Print
+  copy2clipboard(iris_merged)
+  print("Use Ctrl+V to paste summary means into Excel worksheet")
+  
+  # Create standard error bars
+  yerr_names1 <- colnames(data_merged)[c(3,5)]
+  yerrbar1 <- aes_string(ymin = paste(yerr_names1, collapse = '-'), 
+                         ymax = paste(yerr_names1,collapse='+'))
+  nudge_amt <- 0
+  if (mean(data_merged$se) < 1){
+      nudge_amt <- 5*mean(data_merged$se)
+  } else {
+    nudge_amt <- 2*mean(data_merged$se)}
+# Plot
+plot1 <- ggplot(data_merged, aes_string(x=user_group, y=user_treatment, fill=user_group))+
+    geom_bar(position=position_dodge(.5), stat="identity", colour="black", width = 0.5)+
+    geom_errorbar(mapping = yerrbar1, width=0.2)+
+    geom_text(nudge_y=nudge_amt, label=groups,  size=4) +
+    ggtitle("HSD grouping")+
+    theme_uf() # from dpaudelR package
+print(plot1)
+}
