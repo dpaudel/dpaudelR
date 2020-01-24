280c1f94de3e42cc044286c4dd291d7e37f07f86
Dev Paudel <dev.paudel@ufl.edu>
2020-01-24 10:46:16 -0500

    Update data_transform.R

--- R/data_transform.R
+++ R/data_transform.R
@@ -4,9 +4,9 @@
 #' 
 #' @export
 #' dataframe, column_name
-#' data_transform(dataframe, 'column_name')
-#' data_transform(phenotype,'Days_to_pod_maturity')
-data_transform <- function(dataframe, column_name){
+#' data_transform(dataframe, 'column_name', numberOfBins)
+#' data_transform(phenotype,'Days_to_pod_maturity', 30)
+data_transform <- function(dataframe, column_name, numbins){
   newd <- data.frame(dataframe %>% select(column_name))
   newd$log <- log(newd[,1])
   newd$log10 <- log10(newd[,1])
@@ -14,15 +14,13 @@ data_transform <- function(dataframe, column_name){
   if(max(newd[,1]) <=1 & min(newd[,1]>=0)){
   newd$asin_sqrt <- asin(sqrt(newd[,1]))
   }
-  num_cols <- dim(newd)[2]
   newd %>% 
     tidyr::gather(variable, value) %>% 
     ggplot(aes(value)) + 
     # it sets bins intead of breaks, so add 1
-    geom_histogram(bins =(nrow(newd)/10) %>% round(., digits=0), color='black', fill='grey40') + 
+    geom_histogram(bins = numbins, color='black', fill='grey40') + 
     # make a new "facet" for each value of `variable` (formerly column names), and 
-    # use a convenient x-scale instead of the same for all 3
+    # use a convenient x-scale instead of the same for all 
     facet_wrap(~variable, scales = 'free_x')+
     theme_bw()
 }
-
