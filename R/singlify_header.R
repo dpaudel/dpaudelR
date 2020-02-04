cb0a6a1aac24cb078d6d0cb742b9fa4f12a0a23a
Dev Paudel <dev.paudel@ufl.edu>
2020-02-04 13:44:16 -0500

    Update singlify_header.R

--- R/singlify_header.R
+++ R/singlify_header.R
@@ -14,10 +14,10 @@ long_headers = data.frame(t(headers))
 
 # merge headers 
 merged_header <- apply(long_headers, 1, paste, collapse="_")
-
+merged_header1 <- make.names(merged_header, unique=TRUE)
 # Add column names down the columns
 #long_headers <- fill(long_headers,1:nheaders) # all four columns
-headers <- data.frame(t(merged_header)) # back to vertical.
+headers <- data.frame(t(merged_header1)) # back to vertical.
 
 # summarize using str_c from stringr which collapses strings.
 # summarize_all makes it work on all columns.
